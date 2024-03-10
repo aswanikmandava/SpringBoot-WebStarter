3254ec212a3fb230f7925fd20b99002a26836cdc
aswanikmandava <aswani.k.mandava@gmail.com>
2024-03-10 17:39:36 -0400

    Init version of SpringBoot web starter project

---
+++ recommender-api/src/test/java/com/mandava/springboot/recommenderapi/RecommenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandava.springboot.recommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
